d5a1463a3eb259cc0f62ddac0b02b2bf4e863bff
Huy Bui <anhhuybk@gmail.com>
2014-07-10 16:01:11 -0700

    rdma_put works between 2 points

--- ugni/rdma_put.c
+++ ugni/rdma_put.c
@@ -54,37 +54,21 @@ int main(int argc, char **argv)
 	iters = NUMBER_OF_TRANSFERS;
     }
 
-    unsigned int   *all_nic_addresses;
-    int             cookie;
     int             create_destination_cq = 1;
-    int             create_destination_overrun = 0;
-    gni_cq_entry_t  current_event;
     uint64_t        data = SEND_DATA;
-    int             data_transfers_sent = 0;
-    int             device_id = 0;
-    int             event_id;
-    gni_post_descriptor_t *event_post_desc_ptr;
     register int    i;
     register int    j;
-    unsigned int    local_address;
-    int             modes = GNI_CDM_MODE_BTE_SINGLE_CHANNEL;
     int             my_id;
-    //mdh_addr_t      my_memory_handle;
     int             my_receive_from;
     int             rc;
     gni_post_descriptor_t *rdma_data_desc;
     int             receive_from;
     unsigned int    remote_address;
-    //gni_mem_handle_t receive_memory_handle;
-    //mdh_addr_t     *remote_memory_handle_array;
     uint64_t       *receive_buffer;
     uint64_t        receive_data = SEND_DATA;
     uint64_t       *send_buffer;
     int             send_to;
-    //gni_mem_handle_t source_memory_handle;
     gni_return_t    status = GNI_RC_SUCCESS;
-    char           *text_pointer;
-    //gni_ep_handle_t *endpoint_handles_array;
 
     int number_of_cq_entries  = iters;
     int number_of_dest_cq_entries = 1;
@@ -94,31 +78,6 @@ int main(int argc, char **argv)
 
     node.uGNI_createBasicCQ(number_of_cq_entries, number_of_dest_cq_entries);
     node.uGNI_createAndBindEndpoints();
-
-    /*gni_cq_handle_t cq_handle, destination_cq_handle;
-
-    status = GNI_CqCreate(node.nic_handle, number_of_cq_entries, 0, GNI_CQ_NOBLOCK, NULL, NULL, &cq_handle);
-    if (status != GNI_RC_SUCCESS) {
-	fprintf(stdout, "[%s] Rank: %4i GNI_CqCreate source ERROR status: %d\n", uts_info.nodename, node.world_rank, status);
-	INCREMENT_ABORTED;
-	goto EXIT_DOMAIN;
-    }
-
-    status = GNI_CqCreate(node.nic_handle, number_of_dest_cq_entries, 0, GNI_CQ_NOBLOCK, NULL, NULL, &destination_cq_handle);
-    if (status != GNI_RC_SUCCESS) {
-	fprintf(stdout,
-		"[%s] Rank: %4i GNI_CqCreate      destination ERROR status: %d\n",
-		uts_info.nodename, node.world_rank, status);
-	INCREMENT_ABORTED;
-	goto EXIT_CQ;
-    }*/
-
-    /* Allocate the endpoint handles array. */
-   /* endpoint_handles_array = (gni_ep_handle_t *) calloc(node.world_size, sizeof(gni_ep_handle_t));
-    assert(endpoint_handles_array != NULL);
-
-    uGNI_CreateAndBindEndpoints(node.world_rank, node.world_size, node.cq_handle, node.nic_handle, endpoint_handles_array);
-    */
 
     rc = posix_memalign((void **) &send_buffer, 64,
 	    (transfer_length_in_bytes * iters));
@@ -133,35 +92,6 @@ int main(int argc, char **argv)
 
     /*Initialize the buffer to all zeros.*/
     memset(receive_buffer, 0, (transfer_length_in_bytes * iters));
-
-    /*status = GNI_MemRegister(node.nic_handle, (uint64_t) send_buffer,
-	    (transfer_length_in_bytes * iters), NULL,
-	    GNI_MEM_READWRITE, -1,
-	    &source_memory_handle);
-    if (status != GNI_RC_SUCCESS) {
-	fprintf(stdout,
-		"[%s] Rank: %4i GNI_MemRegister  send_buffer ERROR status: %d\n",
-		uts_info.nodename, node.world_rank, status);
-    }
-
-    status = GNI_MemRegister(node.nic_handle, (uint64_t) receive_buffer,
-	    transfer_length_in_bytes * iters, node.destination_cq_handle,
-	    GNI_MEM_READWRITE,
-	    -1, &receive_memory_handle);
-    if (status != GNI_RC_SUCCESS) {
-	fprintf(stdout,
-		"[%s] Rank: %4i GNI_MemRegister   receive_buffer ERROR status: %d\n",
-		uts_info.nodename, node.world_rank, status);
-    }
-
-    remote_memory_handle_array = (mdh_addr_t *) calloc(node.world_size, sizeof(mdh_addr_t));
-    assert(remote_memory_handle_array);
-
-    my_memory_handle.addr = (uint64_t) receive_buffer;
-    my_memory_handle.mdh = receive_memory_handle;
-
-    allgather(&my_memory_handle, remote_memory_handle_array, sizeof(mdh_addr_t));
-    */
 
     node.uGNI_regAndExchangeMem(send_buffer, transfer_length_in_bytes * iters, receive_buffer, transfer_length_in_bytes * iters);
 
@@ -256,12 +186,10 @@ int main(int argc, char **argv)
 		fprintf(stdout, "[%s] Rank: %4i GNI_PostRdma data ERROR status: %d\n", uts_info.nodename, node.world_rank, status);
 		continue;
 	    }
-
-	    data_transfers_sent++;
 	}   /* end of for loop for transfers */
 
 	//Check to make sure that all sends are done.
-	uGNI_WaitAllSendDone(node.world_rank, send_to, node.cq_handle, iters);
+	node.uGNI_waitAllSendDone(send_to, node.cq_handle, iters);
 
 	if(node.world_rank == 0) {
 	    gettimeofday(&t2, NULL);
@@ -273,7 +201,7 @@ int main(int argc, char **argv)
 
     if(node.world_rank == 4) {
 	//Check to get all data received
-	uGNI_WaitAllRecvDone(node.world_rank, receive_from, node.destination_cq_handle, iters);
+	node.uGNI_waitAllRecvDone(receive_from, node.destination_cq_handle, iters);
     }
 
 EXIT_WAIT_BARRIER:
