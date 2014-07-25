6d37ce4fe476d896d989cdec2c83c8e052526eab
Huy Bui <anhhuybk@gmail.com>
2014-07-24 18:48:10 -0700

    up

--- ugni/mpath.c
+++ ugni/mpath.c
@@ -1,8 +1,8 @@
 /*
-* Multiple paths data movement
-* @author: Huy Bui
-* @Date: July 24th, 2014
-*/
+ * Multiple paths data movement
+ * @author: Huy Bui
+ * @Date: July 24th, 2014
+ */
 
 #include <stdio.h>
 #include<stdlib.h>
@@ -27,14 +27,14 @@ int main(int argv, char **argc) {
     node.uGNI_init();    
     node.uGNI_getTopoInfo();
 
-    proc_t *proc = (roc*)malloc(sizeof(Proc));
+    proc_t *proc = (proc_t*)malloc(sizeof(proc_t));
     proc->world_rank = node.world_rank;
     proc->nid = node.nid;
     proc->coord_x = node.coord.mesh_x;
     proc->coord_y = node.coord.mesh_y;
     proc->coord_z = node.coord.mesh_z;
 
-    Proc *procs = (Proc*)malloc(sizeof(Proc)*node.world_size);
+    proc_t *procs = (proc_t*)malloc(sizeof(proc_t)*node.world_size);
     MPI_Allgather(proc, 5, MPI_INT, procs, 5, MPI_INT, MPI_COMM_WORLD); 
 
     if(node.world_rank == 0) {
@@ -43,7 +43,7 @@ int main(int argv, char **argc) {
 	}
     }
 
-    Proc *distinct_procs = (Proc*)malloc(sizeof(Proc)*node.world_size);
+    proc_t *distinct_procs = (proc_t*)malloc(sizeof(proc_t)*node.world_size);
     int num_aries;
 
     distinct_procs[0].world_rank = procs[0].world_rank;
@@ -59,14 +59,14 @@ int main(int argv, char **argc) {
 	existing = false;
 	for(int j = 0; j < num_aries; j++){
 	    if(distinct_procs[j].coord_x == procs[i].coord_x && 
-		distinct_procs[j].coord_y == procs[i].coord_y &&
-		distinct_procs[j].coord_z == procs[i].coord_z) {
-		    existing = true;
-		    break;
-		}
+		    distinct_procs[j].coord_y == procs[i].coord_y &&
+		    distinct_procs[j].coord_z == procs[i].coord_z) {
+		existing = true;
+		break;
+	    }
 	    if(!existing) {
 		num_aries++;
-		distinct_procs[i].world_rank = procs[i].world_rank;
+		distinct_procs[j].world_rank = procs[i].world_rank;
 		distinct_procs[j].coord_x = procs[i].coord_x;
 		distinct_procs[j].coord_y = procs[i].coord_y;
 		distinct_procs[j].coord_z = procs[i].coord_z;
@@ -77,7 +77,7 @@ int main(int argv, char **argc) {
 
     /*Print distinct aries*/
     if(node.world_rank == 0) {
-        for(int i = 0; i < num_aries; i++) {
+	for(int i = 0; i < num_aries; i++) {
 	    printf("Rank %d [%d %d %d %d]\n", distinct_procs[i].world_rank, distinct_procs[i].coord_x, distinct_procs[i].coord_y, distinct_procs[i].coord_z, distinct_procs[i].nid);
 	}
     }
@@ -89,10 +89,104 @@ int main(int argv, char **argc) {
 	MPI_Abort(MPI_COMM_WORLD, 911);
     }
 
-    for(int i = 0; i < num_aries; i++) {
-	
+    if(node.world_rank == distinct_procs[0].world_rank) {
+	node.isSource = true;
+    }
+    if(node.world_rank == distinct_procs[num_aries-1].world_rank) {
+	node.isDest = true;
+    }
+    if(node.world_rank == distinct_procs[num_aries/2].world_rank) {
+	node.isProxy = true;
+    }
+    if(node.world_rank == distinct_procs[num_aries/2-1].world_rank) {
+	node.isProxy = true;
+    }
+    int iters = 30;
+    int nbytes = 4*1024*1024;
+
+    int sourceId = distinct_procs[0].world_rank;
+    int destId = distinct_procs[num_aries-1].world_rank;
+    int proxy1 = distinct_procs[num_aries/2-1].world_rank;
+    int proxy2 = distinct_procs[num_aries/2].world_rank;
+
+    char *send_buf = (char*)malloc(nbytes*iters);
+    char *recv_buf = (char*)malloc(nbytes*iters);
+    MPI_Request *request = (MPI_Request*)malloc(sizeof(MPI_Request)*iters);
+    MPI_Status *status = (MPI_Status*)malloc(sizeof(MPI_Status)*iters);
+
+    struct timeval t1, t2;
+    gettimeofday(&t1, NULL);
+
+    if(node.isSource) {
+	for(int i = 0; i < iters; i++) {
+	    MPI_Isend(&send_buf[i*nbytes], nbytes, MPI_BYTE, destId, 0, MPI_COMM_WORLD, &request[i]);
+	}
+	MPI_Waitall(iters, request, status);
+    }
+
+    if(node.isDest) {
+	for(int i = 0; i < iters; i++) {
+	    MPI_Irecv(&recv_buf[i*nbytes], nbytes, MPI_BYTE, sourceId, 0, MPI_COMM_WORLD, &request[i]);
+	}
+	MPI_Waitall(iters, request, status);
+    }
+
+    gettimeofday(&t2, NULL);
+
+    double latency = ((t2.tv_sec * 1000000 + t2.tv_usec) - (t1.tv_sec * 1000000 + t1.tv_usec))*1.0/iters;
+    double max_latency = 0;
+    MPI_Reduce(&latency, &max_latency, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+    if(node.world_rank == 0) {
+	double bandwidth = nbytes*1000000.0/(max_latency*1024*1024);
+	printf("%d \t %8.6f \t %8.4f\n", nbytes, bandwidth, max_latency);
+    }
+
+    gettimeofday(&t1, NULL);
+
+    if(node.isSource) {
+	for(int i = 0; i < iters/2; i++) {
+            MPI_Isend(&send_buf[i*nbytes], nbytes, MPI_BYTE, proxy1, 0, MPI_COMM_WORLD, &request[i]);
+        }
+	for(int i = iters/2; i < iters; i++) {
+            MPI_Isend(&send_buf[i*nbytes], nbytes, MPI_BYTE, proxy2, 0, MPI_COMM_WORLD, &request[i]);
+        }
+        MPI_Waitall(iters, request, status);
+    }
+
+    if(node.world_rank == proxy1) {
+	for(int i = 0; i < iters/2; i++) {
+            MPI_Recv(&recv_buf[i*nbytes], nbytes, MPI_BYTE, sourceId, 0, MPI_COMM_WORLD, &status[i]);
+	    MPI_Isend(&recv_buf[i*nbytes], nbytes, MPI_BYTE, destId, 0, MPI_COMM_WORLD, &request[i]);
+        }
+	MPI_Waitall(iters/2, request, status);
+    }
+    if(node.world_rank == proxy2) {
+        for(int i = iters/2; i < iters; i++) {
+            MPI_Recv(&recv_buf[i*nbytes], nbytes, MPI_BYTE, sourceId, 0, MPI_COMM_WORLD, &status[i]);
+	    MPI_Isend(&recv_buf[i*nbytes], nbytes, MPI_BYTE, destId, 0, MPI_COMM_WORLD, &request[i]);
+	}
+        MPI_Waitall(iters/2, &request[iters/2], status);
     }
     
+    if(node.isDest) {
+	for(int i = 0; i < iters/2; i++) {            
+	    MPI_Irecv(&recv_buf[i*nbytes], nbytes, MPI_BYTE, proxy1, 0, MPI_COMM_WORLD, &request[i]);
+            MPI_Irecv(&recv_buf[(i+iters/2)*nbytes], nbytes, MPI_BYTE, proxy2, 0, MPI_COMM_WORLD, &request[i+iters/2]);
+        }
+	MPI_Waitall(iters, request, status);
+    }
+
+    gettimeofday(&t2, NULL);
+
+    latency = ((t2.tv_sec * 1000000 + t2.tv_usec) - (t1.tv_sec * 1000000 + t1.tv_usec))*1.0/iters;
+    max_latency = 0;
+    MPI_Reduce(&latency, &max_latency, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+    if(node.world_rank == 0) {
+        double bandwidth = nbytes*1000000.0/(max_latency*1024*1024);
+        printf("%d \t %8.6f \t %8.4f\n", nbytes, bandwidth, max_latency);
+    }
 
     PMI_Finalize();
 
